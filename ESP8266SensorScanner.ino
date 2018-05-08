2f0970fb3b7b143b651904a25c8a858d8fb618c3
dtworth <36244729+dtworth@users.noreply.github.com>
2018-05-08 15:25:16 -0400

    Initial upload
    Uses a webpage to display the Vcc voltage, the state of D0, D2 & D3 and allows one to chose a motor speed.  Uses the transmit pin (D1) which happens to be connected to the blue LED.  Eventually it will drive a transistor to vary the speed of the motor.

--- ESP8266SensorScanner.ino
+++ ESP8266SensorScanner.ino
@@ -20,13 +20,52 @@ SOFTWARE.
 */
 
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include "config.h"
 #include "ScanSensors.h"
 #include "DeliverResults.h"
 
+ESP8266WebServer server(80);
+ADC_MODE(ADC_VCC);      // set A0 to read VCC
+
+void handleRoot()
+{
+  char buff[20];
+
+  int vccVoltage = ESP.getVcc();
+  int speed = server.arg(0).toInt();
+  if( speed < 0 )
+    speed = 0;
+  else if( speed > 1023 )
+    speed = 1023;
+  analogWrite(1, 1023 - speed);    // use the Tx pin for output
+  String message = "<html><body>\n"; 
+  message += "<h1>Car 01</h1>\n"; 
+  message += "<form method='GET'>\n";
+  message += "<p>Vcc=";  
+  message += itoa( vccVoltage, buff, 10 );
+  message += "</p>";  
+  message += "<p>D0=";  
+  message += digitalRead(0) ? "1": "0";  
+  message += "</p>";  
+  message += "<p>D2=";  
+  message += digitalRead(2) ? "1": "0";  
+  message += "</p>";
+  message += "<p>D3(Rx)=";  
+  message += digitalRead(3) ? "1": "0";  
+  message += "</p>";  
+  message += "<p>Speed (0-1023): <input type='text' value='";
+  message += itoa( speed, buff, 10 );
+  message += "' name='speed'/></p>";
+  message += "<input type='submit' value='Update'/>";
+  message += "</form></body></html>\n";
+  server.send(200, "text/html", message);
+}
+
+
 void setup() {
-  Serial.begin(115200);
-  Serial.println();
+  server.on( "/", handleRoot );
+  server.begin();
 
   ScanSensors::init();
   DeliverResultsManager::init();
@@ -40,4 +79,5 @@ void loop() {   // scan for strongest signal and send to server
   if( (blockNum = ScanSensors::scan()) >= 0 ) {
     DeliverResultsManager::send( blockNum );
   }
+  server.handleClient();
 }
